26e53b8b9b9d5a4764e393c1a4e16870d60e2e8c
b4bergi <berghammerdomi@gmail.com>
2020-01-31 18:01:41 +0100

    edited comments

--- stack_test.asm
+++ stack_test.asm
@@ -1,38 +1,38 @@
 mov ah, 0x0e 
 
-mov bp, 0x8000 ; this is an address far away from 0x7c00 so that we don't get overwritten
-mov sp, bp ; if the stack is empty then sp points to bp
+mov bp, 0x8000
+mov sp, bp 
 
+; push on stack
 push 'A'
 push 'B'
 push 'C'
 
-; to show how the stack grows downwards
-mov al, [0x7ffe] ; 0x8000 - 2
+mov al, [0x7ffe] 
 int 0x10
 
-; however, don't try to access [0x8000] now, because it won't work
-; you can only access the stack top so, at this point, only 0x7ffe (look above)
+
+
 mov al, [0x8000]
 int 0x10
 
 
-; recover our characters using the standard procedure: 'pop'
-; We can only pop full words so we need an auxiliary register to manipulate
-; the lower byte
+
+
+; pop stack and interupt to show
 pop bx
 mov al, bl
-int 0x10 ; prints C
+int 0x10 
 
 pop bx
 mov al, bl
-int 0x10 ; prints B
+int 0x10 
 
 pop bx
 mov al, bl
-int 0x10 ; prints A
+int 0x10 
 
-; data that has been pop'd from the stack is garbage now
+
 mov al, [0x8000]
 int 0x10
 
