69fc890439cbfb321b73d35f5def18ef33114237
b4bergi <berghammerdomi@gmail.com>
2020-02-05 23:08:14 +0100

    finished boot sector disk, renamed file

--- boot_sect/boot_sect_main.asm
+++ boot_sect/boot_sect_main.asm
@@ -6,10 +6,18 @@
 
     call disk_load
 
+    mov dx, [0x9000] ; first loaded word
+    call print_hex
 
+    call print_nl
 
-%include "../boot_sect_functions/boot_sect_print.asm"
-%include "../boot_sect_functions/boot_sect_print_hex.asm"
+    mov dx, [0x9000 + 512] ; first word from second sector
+    call print_hex
+
+    jmp $
+
+%include "boot_sect_functions/boot_sect_print.asm"
+%include "boot_sect_functions/boot_sect_print_hex.asm"
 %include "boot_sect_disk.asm"
 
 times 510 - ($-$$) db 0
