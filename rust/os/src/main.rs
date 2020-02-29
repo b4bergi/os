dc63ce701a5aa97e16d61cc2593d506c75a05e21
b4bergi <berghammerdomi@gmail.com>
2020-02-29 16:41:28 +0100

    implemented test methods

--- rust/os/src/main.rs
+++ rust/os/src/main.rs
@@ -1,5 +1,8 @@
 #![no_std] // no standard library --> no println!("Hello, world!");
 #![no_main] //disable rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
@@ -7,12 +10,32 @@ mod vga_buffer;
 
 // function is called on panic:
 #[panic_handler]
-fn panic(_info: &PanicInfo)-> ! {
+fn panic(info: &PanicInfo)-> ! {
+    println!("{}", info);
     loop{}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {}", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
