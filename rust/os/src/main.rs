f706390851e8a633f8866289122677fe85c75de2
b4bergi <berghammerdomi@gmail.com>
2020-03-01 23:23:01 +0100

    create lib and use it in main

--- rust/os/src/main.rs
+++ rust/os/src/main.rs
@@ -1,30 +1,11 @@
-#![no_std] // no standard library --> no println!("Hello, world!");
-#![no_main] //disable rust-level entry points
+#![no_std]
+#![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use os::println;
 use core::panic::PanicInfo;
-
-mod vga_buffer;
-mod serial;
-
-// function is called on panic:
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo)-> ! {
-    println!("{}", info);
-    loop{}
-}
-
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -36,34 +17,16 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+/// function called on panic
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {}", tests.len());
-    for test in tests {
-        test();
-    }
-    exit_qemu(QemuExitCode::Success)
-}
-
-#[test_case]
-fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
-    assert_eq!(1, 1);
-    serial_println!("[ok]");
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    os::test_panic_handler(info)
 }
