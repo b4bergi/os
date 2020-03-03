ecc5e9f644daa619c2e48ed11803789d063b1209
b4bergi <berghammerdomi@gmail.com>
2020-03-03 22:08:50 +0100

    simplified should_panic test

--- rust/os/tests/should_panic.rs
+++ rust/os/tests/should_panic.rs
@@ -1,15 +1,14 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 use os::{QemuExitCode, exit_qemu, serial_println};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    test_main();
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
 
     loop {}
 }
@@ -21,17 +20,6 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
 fn should_fail() {
     serial_println!("should fail ...");
     assert_eq!(0, 1);
