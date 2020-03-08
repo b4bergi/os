c67c4cf8c5a35817089a36c027b865451db3a444
b4bergi <berghammerdomi@gmail.com>
2020-03-08 20:09:46 +0100

    implement double fault exception

--- rust/os/src/interrupts.rs
+++ rust/os/src/interrupts.rs
@@ -9,6 +9,7 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.double_fault.set_handler_fn(double_fault_handler);
         idt
     }; 
 }
@@ -27,4 +28,8 @@ fn test_breakpoint_exception() {
     serial_print!("test_breakpoint_exception...");
     x86_64::instructions::interrupts::int3();
     serial_println!("[ok]");
+}
+
+extern "x86-interrupt" fn double_fault_handler(stack_frame: &mut InterruptStackFrame, _error_code: u64) -> ! {
+    panic!("EXCEPTION: Double fault\n{:#?}", stack_frame);
 }
