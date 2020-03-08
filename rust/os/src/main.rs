f3eae7ccc47a65889f4784d2c5834503c241d498
b4bergi <berghammerdomi@gmail.com>
2020-03-08 20:28:39 +0100

    stack overflow and Task state segment

--- rust/os/src/main.rs
+++ rust/os/src/main.rs
@@ -13,10 +13,12 @@ pub extern "C" fn _start() -> ! {
 
     os::init();
 
-    // trigger a page fault
-    unsafe {
-        *(0xdeadbeef as *mut u64) = 42;
+    fn stack_overflow(){
+        stack_overflow();
     }
+
+    //trigger a stack overflow
+    stack_overflow();
 
     #[cfg(test)]
     test_main();
