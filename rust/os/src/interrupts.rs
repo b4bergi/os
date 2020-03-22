5cec6827ba4a3585370c9962c6134588cdd69792
b4bergi <berghammerdomi@gmail.com>
2020-03-22 23:45:05 +0100

    added interrupt timer

--- rust/os/src/interrupts.rs
+++ rust/os/src/interrupts.rs
@@ -1,6 +1,7 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 use lazy_static::lazy_static;
 use crate::println;
+use crate::print;
 #[cfg(test)]
 use crate::{serial_print, serial_println};
 use crate::gdt;
@@ -21,6 +22,8 @@ lazy_static! {
         unsafe {
             idt.double_fault.set_handler_fn(double_fault_handler).set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
+        idt[InterruptIndex::Timer.as_usize()]
+        .set_handler_fn(timer_interrupt_handler);
 
         idt
     }; 
@@ -44,4 +47,24 @@ fn test_breakpoint_exception() {
 
 extern "x86-interrupt" fn double_fault_handler(stack_frame: &mut InterruptStackFrame, _error_code: u64) -> ! {
     panic!("EXCEPTION: Double fault\n{:#?}", stack_frame);
+}
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+    Timer = PIC_1_OFFSET,
+}
+
+impl InterruptIndex {
+    fn as_u8(self) -> u8 {
+        self as u8
+    }
+
+    fn as_usize(self) -> usize {
+        usize::from(self.as_u8())
+    }
+}
+
+extern "x86-interrupt" fn timer_interrupt_handler (_stack_frame: &mut InterruptStackFrame) {
+    print!(".");
 }
